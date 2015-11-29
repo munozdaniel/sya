71e6a12779890d743eba0260e4481b8a36ec4a88
Munoz Daniel <munozda87@hotmail.com>
2015-11-29 15:15:26 -0300

    index: Inicio de sesion. Recuperar contraseña.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,13 +1,20 @@
 <?php
 
+/**
+ * Class IndexController
+ * Inicio del sistema, donde se solicitan los datos para ingresar. Se encarga de validar y chequear los permisos.
+ * Permite recuperar los datos, en caso que se hayan olvidado, se les envia un correo con dichos datos recuperados desde
+ * la base de datos.
+ */
 class IndexController extends ControllerBase
 {
     public function initialize()
     {
-        $this->tag->setTitle('Bienvenidos');
+        $this->tag->setTitle('Iniciar Sesión');
         parent::initialize();
 
     }
+
     /**
      * Inicio de Sesion: Solicita usuario y contraseña.
      */
@@ -21,69 +28,59 @@ class IndexController extends ControllerBase
      */
     public function validarAction()
     {
-        if($this->request->isPost())
-        {
-            try
-            {
+        if ($this->request->isPost()) {
+            try {
                 //Obtengo los datos ingresado en el form.
-                $nombre  = $this->request->getPost('sesion_nombre','string');
-                $pass   = $this->request->getPost('sesion_contrasena','alphanum');
+                $nombre = $this->request->getPost('sesion_nombre', 'string');
+                $pass = $this->request->getPost('sesion_contrasena', 'alphanum');
 
                 //Busco el usuario en la bd a partir de los datos ingresados.
-                $usuarios =  Usuarios::findFirst(array(
+                $usuarios = Usuarios::findFirst(array(
                     "(usuario_nick = :usuario_nick:) AND (usuario_contrasenia = :usuario_contrasenia:) AND (usuario_activo = 1)",
                     'bind' => array('usuario_nick' => $nombre, 'usuario_contrasenia' => base64_encode($pass))
                 ));
 
-                if($usuarios!=false)
-                {
-                    if($this->_registrarSesion($usuarios))
-                    {
+                if ($usuarios != false) {
+                    if ($this->_registrarSesion($usuarios)) {
                         $miSesion = $this->session->get('auth');
                         $this->flash->message('exito', "Bienvenido/a " . $miSesion['usuario_nombreCompleto'] . " - Rol: " . $miSesion['rol_nombre']);
                         //Redireccionar la ejecución si el usuario es valido
-                        return $this->redireccionar('administrar/index');
+                        return $this->redireccionar('dashboard/index');
+                    } else {
+                        $this->flash->warning("<p>EL USUARIO NO TIENE LOS PERMISOS NECESARIOS PARA INGRESAR</p>");
+                        return $this->redireccionar('index/index');
                     }
-                    else
-                    {
-                        $this->flash->message('aviso',"<p>EL USUARIO NO TIENE LOS ROLES NECESARIOS PARA ADMINISTRAR</p>");
-                        return $this->redireccionar('sesion/index');
-                    }
+                } else {
+                    $this->flash->message('problema', "<p>No se encontro el Usuario, verifique contraseña/nick</p>");
                 }
-                else
-                {
-                    $this->flash->message('problema',"<p>No se encontro el Usuario, verifique contraseña/nick</p>");
-                }
-            }
-            catch(\Phalcon\Annotations\Exception $ex)
-            {
+            } catch (\Phalcon\Annotations\Exception $ex) {
                 $this->flash->error($ex->getMessage());
             }
 
         }
-        return $this->redireccionar('sesion/index');
+        return $this->redireccionar('index/index');
     }
 
     private function _registrarSesion($usuario)
     {
         $idRol = Usuarioporrol::findFirst(array(
             "usuario_id     =       :usuario:",
-            'bind'          =>      array('usuario'=>$usuario->usuario_id)
+            'bind' => array('usuario' => $usuario->usuario_id)
         ));
 
-        if(!$idRol)//No se encontro el rol asignado al usuario
+        if (!$idRol)//No se encontro el rol asignado al usuario
             return false;
-        else
-        {
+        else {
             $rol = Rol::findFirstByRolId($idRol->rol_id);
-
-            $this->session->set('auth',array('usuario_id'   =>  $usuario->usuario_id,
-                'usuario_nombreCompleto'  =>  $usuario->usuario_nombreCompleto,
-                'usuario_nick'  =>  $usuario->usuario_nick,
-                'rol_nombre'   =>  $rol->rol_nombre));
+            echo "" . $usuario->usuario_id . " - " . $rol->rol_nombre;
+            $this->session->set('auth', array('usuario_id' => $usuario->usuario_id,
+                'usuario_nombreCompleto' => $usuario->usuario_nombreCompleto,
+                'usuario_nick' => $usuario->usuario_nick,
+                'rol_nombre' => $rol->rol_nombre));
             return true;
         }
     }
+
     /**
      * Finishes the active session redirecting to the index
      *
@@ -95,45 +92,46 @@ class IndexController extends ControllerBase
         $this->flash->success('Se ha cerrado la sesión.');
         return $this->redireccionar("index/index");
     }
+
     /**
      * Busca con el email del empleado los datos usuario y contraseña, y los envia al correo ingresado por formulario.
      */
     public function recuperarAction()
-    {   $this->view->pick('sesion/index');
-        if($this->request->isPost())
-        {
-            $email  = $this->request->getPost('sesion_email');
+    {
+        $this->view->pick('index/index');
+        if ($this->request->isPost()) {
+            $email = $this->request->getPost('sesion_email');
             $usuario = Usuarios::findFirstByUsuario_email($email);
-            if($usuario)
-            {
-                $this->mail->CharSet        = 'UTF-8' ;
-                $this->mail->Host           = 'mail.imps.org.ar';
-                $this->mail->SMTPAuth       = true;
-                $this->mail->Username       = 'desarrollo@imps.org.ar';
-                $this->mail->Password       = 'sis$%&--temas';
-                $this->mail->SMTPSecure     = '';
-                $this->mail->Port           = 26;
+            if ($usuario) {
+                $this->mail->CharSet = 'UTF-8';
+                $this->mail->Host = 'mail.imps.org.ar';
+                $this->mail->SMTPAuth = true;
+                $this->mail->Username = 'desarrollo@imps.org.ar';
+                $this->mail->Password = 'sis$%&--temas';
+                $this->mail->SMTPSecure = '';
+                $this->mail->Port = 26;
                 $this->mail->addAddress($email);
 
 
-                $this->mail->From  = "desarrollo@imps.org.ar";
-                $this->mail->FromName   =  "Soporte Tecnico";
+                $this->mail->From = "desarrollo@imps.org.ar";
+                $this->mail->FromName = "Soporte Tecnico";
                 //$this->mail->addReplyTo("munozda87@hotmail.com", "user");
-                $this->mail->Subject        =   "Recuperar Contraseña";
-                $mensaje = "Se ha solicitado la contraseña y el usuario para acceder al <em>Sistema Web IMPS</em>. <br>En caso de que usted no lo haya solicitado, por favor borre este email y comuniquese con <em>Soporte Tecnico IMPS</em> via email: <b>desarrollo@imps.org.ar</b>.<br><br> <b>USUARIO</b>:   ".$usuario->usuario_nick."<br> <b>CONTRASEÑA</b>:". base64_decode($usuario->usuario_contrasenia);
-                $this->mail->Body           =   $mensaje;
-                if($this->mail->send())
-                    $this->flash->message('exito',"<p>Su solicitud fue procesada con éxito. En minutos recibirá un mail con su nueva
+                $this->mail->Subject = "Recuperar Contraseña";
+                $mensaje = "Se ha solicitado la contraseña y el usuario para acceder al <em>Sistema Web IMPS</em>. <br>En caso de que usted no lo haya solicitado, por favor borre este email y comuniquese con <em>Soporte Tecnico IMPS</em> via email: <b>desarrollo@imps.org.ar</b>.<br><br> <b>USUARIO</b>:   " . $usuario->usuario_nick . "<br> <b>CONTRASEÑA</b>:" . base64_decode($usuario->usuario_contrasenia);
+                $this->mail->Body = $mensaje;
+                if ($this->mail->send())
+                    $this->flash->message('exito', "<p>Su solicitud fue procesada con éxito. En minutos recibirá un mail con su nueva
 contraseña.</p>");
                 else
-                    $this->flash->message('problema',"<p>Ha sucedido un error. No es posible comunicarse con nuestras oficinas momentáneamente.</p>");
+                    $this->flash->message('problema', "<p>Ha sucedido un error. No es posible comunicarse con nuestras oficinas momentáneamente.</p>");
 
-                $this->redireccionar('sesion/index');
-            }
-            else{
-                $this->flash->message('problema',"<p>No se encuentra registrado ningún usuario con el correo: $email</p>");
+                $this->redireccionar('index/index');
+            } else {
+                $this->flash->message('problema', "<p>No se encuentra registrado ningún usuario con el correo: $email</p>");
             }
         }
     }
+
+
 }
 
--- app/views/index/index.volt
+++ app/views/index/index.volt
@@ -8,21 +8,21 @@
 
             <!-- Login wrapper -->
             <div class="login-wrapper">
-                <form action="#" role="form">
-
+                {{ form('index/validar','method':'post') }}
+                    {{ content() }}
                     <div class="panel panel-default">
                         <div class="panel-heading"><h6 class="panel-title"><i class="fa fa-user"></i> Bienvenido</h6>
                         </div>
                         <div class="panel-body">
                             <div class="form-group has-feedback">
                                 <label>Usuario</label>
-                                <input type="text" class="form-control" placeholder="Nombre de Usuario">
+                                <input id="sesion_nombre" name="sesion_nombre" type="text" class="form-control" placeholder="Nombre de Usuario">
                                 <i class="fa fa-user form-control-feedback"></i>
                             </div>
 
                             <div class="form-group has-feedback">
                                 <label>Contraseña</label>
-                                <input type="password" class="form-control" placeholder="Contraseña">
+                                <input id="sesion_contrasena" name="sesion_contrasena" type="password" class="form-control" placeholder="Contraseña">
                                 <i class="fa fa-lock form-control-feedback"></i>
                             </div>
 
@@ -41,6 +41,11 @@
                                         Iniciar Sesión
                                     </button>
                                 </div>
+                                <div class="col-xs-12">
+                                    <hr>
+                                    <!-- Button HTML (to Trigger Modal) -->
+                                    <a href="#recuperarDatos" role="button" class="btn btn-block btn-primary" data-toggle="modal">Olvidó su contraseña?</a>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -48,10 +53,44 @@
             </div>
             <!-- /login wrapper -->
 
-
         </div>
         <!-- /page content -->
 
     </div>
+    <!--=========== RecuperarContraseña ================-->
+    <div id="recuperarDatos" class="modal fade">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title"><i class="fa fa-envelope-o"></i> RECUPERAR CONTRASEÑA</h4>
+                </div>
+                <div class="modal-body margin-left-right-one"style="border-left: 0 !important; border-right: 0 !important;">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 ">
+                            <!-- START SUBSCRIBE HEADING -->
+                            <div class="heading">
+                                <h2 class="wow fadeInLeftBig">Olvidaste tu contraseña o tu usuario? </h2>
+                                <p>La contraseña de inicio de sesión evita el acceso no autorizado al Sistema. Si no recuerda su contraseña de inicio de sesión, ingrese
+                                    su dirección de correo provista por el equipo de desarrollo, y automáticamente se le enviarán los datos a su casilla de mensajes.</p>
+                            </div>
+                            <!--  FORM -->
+                            {{ form('index/recuperar',"class":"subscribe_form") }}
+                            <div class="subscrive_group wow fadeInUp">
+                                {{ email_field('sesion_email','class':'form-control subscribe_mail','placeholder':'INGRESE SU CORREO ELECTRONICO') }}
+                                <hr>
+                                {{ submit_button('class':' btn-warning btn-lg btn-block','value':'SOLICITAR') }}
+                            </div>
+                            {{ end_form() }}
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-success btn-lg" data-dismiss="modal"> CERRAR  <i class="fa  fa-sign-out pull-right"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--=========== FIN:RecuperarContraseña ================-->
 </section>
 <!-- page container -->
