4dffb198399ffea70c17cae2f37c585f2749265c
Munoz Daniel <munozda87@hotmail.com>
2015-12-09 21:35:46 -0300

    usuariosController: Agregado el header.

--- app/controllers/UsuariosController.php
+++ app/controllers/UsuariosController.php
@@ -1,12 +1,27 @@
 <?php
 
-class UsuariosController extends \Phalcon\Mvc\Controller
+/**
+ * Class UsuariosController
+ * Encargado de gestionar los usuarios que van a utilizar el sistema.
+ */
+class UsuariosController extends ControllerBase
 {
+    public function initialize()
+    {
+        $this->view->setTemplateAfter('principal');
+        $this->tag->setTitle('Perfil del Usuario');
+        $this->assets->collection('header')->addJs('js/application_blank.js');
+        parent::initialize();
 
+    }
+    /**
+     * Muestra el perfil del usuario logueado.
+     */
     public function indexAction()
     {
 
     }
+
 
 }
 
