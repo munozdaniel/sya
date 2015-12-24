a39eb3ebeaf8c413d309ba8d745ef9f2e8a61c30
Munoz Daniel <munozda87@hotmail.com>
2015-12-24 13:41:07 -0300

    DataList: Correccion de javascript. Control undefined

--- app/library/componentes/DataListElement.php
+++ app/library/componentes/DataListElement.php
@@ -20,6 +20,9 @@ class DataListElement extends \Phalcon\Forms\Element implements \Phalcon\Forms\E
      * [0]: Contiene un arreglo con los atributos que puede tener el input.
      * [1]: Contiene una tabla de la BD
      * [2]: Contiene los campos que va a componer el DataList.
+     * [3]: El ID del campo Hidden que se va a utilizar para recuperar la clave del elemento seleccionado.
+     * Datos a tener en cuenta: El boton submit en el formulario debe tener un id "submit"
+     *
      * @param null $attributes
      * @return string
      */
@@ -28,24 +31,39 @@ class DataListElement extends \Phalcon\Forms\Element implements \Phalcon\Forms\E
         $atributosInput = $this->getAttributes ()[0];
         $atributosLista = $this->getAttributes ()[1];
         $campos = $this->getAttributes ()[2];
-        $html = "<input type='text' id='" . $this->getName() ."' name='" . $this->getName() ."' list='list_" . $this->getName()."'";
+        $claveCampo = $this->getAttributes ()[3];
+
+        $nombre = $this->getName();
+        $listNombre = "list_".$nombre;
+
+        $html = "<input type='text' id='$nombre' name='$nombre' list='$listNombre'";
         foreach($atributosInput as $atributo => $valor)
         {
             $html .= " $atributo = ' $valor '";
         }
         $html .= ">\n ";
-        $html .= "<datalist  id=\"list_" . $this->getName() ."\"  >";
+        $html .= "<datalist  id=\"" . $listNombre ."\"  >";
         foreach($atributosLista as $option => $valor)
         {
             $html .= "<option data-value=\"" . $valor->$campos[0]. "\" value=\"".$valor-> $campos[1]."\">";
             $html .= "</option> \n ";
         }
         $html .="</datalist>";
-        $html .= "<input type='hidden' id=".$this->getName()."_field name=".$this->getName()."_field >";
-        $html .= "\n <script>";
-        $html .= "\n   $(document).ready(function () { \n $('#submit').click(function () {\n ";
-        $html .= "var value = $('#".$this->getName()."').val()\n ";
-        $html .= "document.getElementById('".$this->getName()."_field').value = $('#list_".$this->getName()." [value=' + value + ']').data('value') ; }); \n});\n   </script>";
+        $html .= "<input type='text' id=".$claveCampo." name=".$claveCampo." >";
+        $html .= "\n<script>\n";
+        $html .= "$(document).ready(function () {\n";
+        $html .= "$('#submit').click(function () {\n";
+        $html .= "var value = $('#".$nombre."').val();\n";
+        $html .= "var clave = $('#".$listNombre." [value=\"' + value + '\"]').data('value');\n";
+        $html .= "if (typeof clave != 'undefined')\n";
+        $html .= "document.getElementById('".$claveCampo."').value = clave ;\n";
+        $html .= " });\n";
+        $html .= "});\n";
+        $html .= "</script>\n";
+        //$html .= "\n <script>";
+        //$html .= "\n   $(document).ready(function () { \n $('#submit').click(function () {\n alert('document.getElementById('".$this->getName()."_field').value');\n ";
+        //$html .= "var value = $('#".$this->getName()."').val();\n ";
+        //$html .= "document.getElementById('".$this->getName()."_field').value = $('#list_".$this->getName()." [value=' + value + ']').data('value') ; }); \n});\n   </script>";
         return $html;
     }
 }
