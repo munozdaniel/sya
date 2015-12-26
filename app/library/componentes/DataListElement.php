44488c3c894c0e2ac34092ec634ad298e3333cc4
Munoz Daniel <munozda87@hotmail.com>
2015-12-26 11:54:28 -0300

    DataListElement: Permitiendo datalist nulo.

--- app/library/componentes/DataListElement.php
+++ app/library/componentes/DataListElement.php
@@ -16,9 +16,14 @@ class DataListElement extends \Phalcon\Forms\Element implements \Phalcon\Forms\E
     }
 
     /**
-     * El atributo que ingresa es un arreglo de tres elementos, sin contar el id que se puede recuperar con $this->getName()
+     * * El datalist estará compuesto por:
+     * 1ero: un input para escribir el campo buscado.
+     * 2do: un datalist, con los datos del modelo.
+     * 3ero: un hiddenfield, donde se guardará el id del item seleccionado.
+     *
+     * $attributes
      * [0]: Contiene un arreglo con los atributos que puede tener el input.
-     * [1]: Contiene una tabla de la BD
+     * [1]: Contiene una tabla de la BD (El Modelo)
      * [2]: Contiene los campos que va a componer el DataList.
      * [3]: El ID del campo Hidden que se va a utilizar para recuperar la clave del elemento seleccionado.
      * Datos a tener en cuenta: El boton submit en el formulario debe tener un id "submit"
@@ -29,9 +34,9 @@ class DataListElement extends \Phalcon\Forms\Element implements \Phalcon\Forms\E
     public function render($attributes = null)
     {
         $atributosInput = $this->getAttributes ()[0];
-        $atributosLista = $this->getAttributes ()[1];
-        $campos = $this->getAttributes ()[2];
-        $claveCampo = $this->getAttributes ()[3];
+        $atributosModelo = $this->getAttributes ()[1];
+        $columnas = $this->getAttributes ()[2];
+        $idHidden = $this->getAttributes ()[3];
 
         $nombre = $this->getName();
         $listNombre = "list_".$nombre;
@@ -43,20 +48,22 @@ class DataListElement extends \Phalcon\Forms\Element implements \Phalcon\Forms\E
         }
         $html .= ">\n ";
         $html .= "<datalist  id=\"" . $listNombre ."\"  >";
-        foreach($atributosLista as $option => $valor)
-        {
-            $html .= "<option data-value=\"" . $valor->$campos[0]. "\" value=\"".$valor-> $campos[1]."\">";
-            $html .= "</option> \n ";
+        if($atributosModelo != null){
+            foreach($atributosModelo as $option => $valor)
+            {
+                $html .= "<option data-value=\"" . $valor->$columnas[0]. "\" value=\"".$valor-> $columnas[1]."\">";
+                $html .= "</option> \n ";
+            }
         }
         $html .="</datalist>";
-        $html .= "<input type='hidden' id=".$claveCampo." name=".$claveCampo." >";
+        $html .= "<input type='hidden' id=".$idHidden." name=".$idHidden." >";
         $html .= "\n<script>\n";
         $html .= "$(document).ready(function () {\n";
         $html .= "$('#submit').click(function () {\n";
         $html .= "var value = $('#".$nombre."').val();\n";
         $html .= "var clave = $('#".$listNombre." [value=\"' + value + '\"]').data('value');\n";
         $html .= "if (typeof clave != 'undefined')\n";
-        $html .= "document.getElementById('".$claveCampo."').value = clave ;\n";
+        $html .= "document.getElementById('".$idHidden."').value = clave ;\n";
         $html .= " });\n";
         $html .= "});\n";
         $html .= "</script>\n";
